3470af4132a8a9f897d234df42ac9a7e00f5cec2
Jefferson Fragoso <jeffersonfragoso@outlook.com>
2018-07-11 13:26:50 -0300

    Criando entidade Produto e associação muitos para muitos com Categoria

--- src/main/java/com/jefferson/api/FullstackApiApplication.java
+++ src/main/java/com/jefferson/api/FullstackApiApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.jefferson.api.domain.Categoria;
+import com.jefferson.api.domain.Produto;
 import com.jefferson.api.repositories.CategoriaRepository;
+import com.jefferson.api.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class FullstackApiApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackApiApplication.class, args);
@@ -26,9 +30,18 @@ public class FullstackApiApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
-	}
-	
-	
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+	}	
 }
