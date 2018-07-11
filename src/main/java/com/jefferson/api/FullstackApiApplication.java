6c12c0c665a020bfbce5f895acf09567eb374575
Jefferson Fragoso <jeffersonfragoso@outlook.com>
2018-07-11 13:01:50 -0300

    Criando operação de instanciação

--- src/main/java/com/jefferson/api/FullstackApiApplication.java
+++ src/main/java/com/jefferson/api/FullstackApiApplication.java
@@ -1,12 +1,34 @@
 package com.jefferson.api;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FullstackApiApplication {
+import com.jefferson.api.domain.Categoria;
+import com.jefferson.api.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class FullstackApiApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackApiApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+	}
+	
+	
 }
