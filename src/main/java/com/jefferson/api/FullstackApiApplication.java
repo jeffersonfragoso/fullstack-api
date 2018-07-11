d2f9dcbe77b05392230fe68c72ad0d72cce50ffc
Jefferson Fragoso <jeffersonfragoso@outlook.com>
2018-07-11 10:48:54 -0300

    Projeto criado

---
+++ src/main/java/com/jefferson/api/FullstackApiApplication.java
@@ -0,0 +1,12 @@
+package com.jefferson.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackApiApplication.class, args);
+	}
+}
