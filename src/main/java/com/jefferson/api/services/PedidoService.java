84bef6328f65da03681bfcd529e3eddca96e56a5
Jefferson Fragoso <jeffersonfragoso@outlook.com>
2018-07-11 21:13:34 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/jefferson/api/services/PedidoService.java
@@ -0,0 +1,22 @@
+package com.jefferson.api.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jefferson.api.domain.Pedido;
+import com.jefferson.api.repositories.PedidoRepository;
+import com.jefferson.api.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);  
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: "+ id + ", Tipo: " +Pedido.class.getName()));
+	}
+}
